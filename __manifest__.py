7d09188b7495f3fa14f04d76f2d342c24699b198
Amarendra Singh <48523377+amarendra-singh@users.noreply.github.com>
2022-11-08 07:23:24 +0000

    project init

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+  'name': 'hospital Management',
+  'version': '1.0.0',
+  'category': '',
+  'summary': '',
+  'decription': '',
+  'depends': [],
+  'data': [],
+  'demo': [],
+  'installable': True,
+  'auto_install': False,
+  'assets': {},
+}
